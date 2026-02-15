2f4e4ca8e699d4d16f62f0164c578f84cbe6b7ca
Ashkar S <ashkars178@gmail.com>
2026-02-15 11:45:39 +0530

    learning redux

--- src/Redux/Slices/CartSlice.js
+++ src/Redux/Slices/CartSlice.js
@@ -6,44 +6,66 @@ const CartSlice = createSlice({
   initialState: {
     CartData: [],
   },
-  reducers:{
-    addtoCart:((state,argToAdd)=>{
-      let existingData=state.CartData.find((eachPro)=>eachPro.id==argToAdd.payload.id)
-      if(existingData){
-        existingData.quantity++
-        existingData.totalPrice=existingData.quantity*existingData.price
+  reducers: {
+    addtoCart: (state, argToAdd) => {
+      let existingData = state.CartData.find(
+        (eachPro) => eachPro.id == argToAdd.payload.id,
+      );
+      if (existingData) {
+        existingData.quantity++;
+        existingData.totalPrice = existingData.quantity * existingData.price;
 
-        let remainingProduct=state.CartData.filter((dataEach)=>dataEach.id!=existingData.id)
-        state.CartData=[...remainingProduct,existingData]
-      }else{
+        let remainingProduct = state.CartData.filter(
+          (dataEach) => dataEach.id != existingData.id,
+        );
+        state.CartData = [...remainingProduct, existingData];
+      } else {
         // state.CartData.push(argToAdd.payload)
-        state.CartData=[...state.CartData,{...argToAdd.payload,quantity:1,totalPrice:argToAdd.payload.price}]
-        alert('successfully Added')
+        state.CartData = [
+          ...state.CartData,
+          {
+            ...argToAdd.payload,
+            quantity: 1,
+            totalPrice: argToAdd.payload.price,
+          },
+        ];
+        alert("successfully Added");
       }
-    }),
-     reduceFromCart:((state,removeCart)=>{
-      let existingData=state.CartData.find((state)=>state.id==removeCart.payload.id)
-       if(existingData){
-        existingData.quantity--
-        existingData.totalPrice=existingData.quantity*existingData.price
+    },
+    reduceFromCart: (state, removeCart) => {
+      let existingData = state.CartData.find(
+        (state) => state.id == removeCart.payload.id,
+      );
+      if (existingData) {
+        existingData.quantity--;
+        existingData.totalPrice = existingData.quantity * existingData.price;
 
-        let remainingProduct=state.CartData.filter((dataEach)=>dataEach.id!=existingData.id)
-        state.CartData=[...remainingProduct,existingData]
-      }else{
+        let remainingProduct = state.CartData.filter(
+          (dataEach) => dataEach.id != existingData.id,
+        );
+        state.CartData = [...remainingProduct, existingData];
+      } else {
         // state.CartData.push(argToAdd.payload)
-        state.CartData=[...state.CartData,{...argToAdd.payload,quantity:1,totalPrice:argToAdd.payload.price}]
-     
+        state.CartData = [
+          ...state.CartData,
+          {
+            ...argToAdd.payload,
+            quantity: 1,
+            totalPrice: argToAdd.payload.price,
+          },
+        ];
       }
-     }),
-     deleteCart:((state,iddel)=>{
-      let remP=state.CartData.filter((eachP)=>{
-        eachP.id!=iddel.payload
-      })
-     }),
-     emptyCart:((state,idEmp)=>{
-      state.CartData=[]
-     })
-  }
+    },
+    deleteCart: (state, iddel) => {
+      let remP = state.CartData.filter((eachP) => {
+        eachP.id != iddel.payload;
+      });
+    },
+    emptyCart: (state, idEmp) => {
+      state.CartData = [];
+    },
+  },
 });
-export const {addtoCart,reduceFromCart,deleteCart,emptyCart}=CartSlice.actions
+export const { addtoCart, reduceFromCart, deleteCart, emptyCart } =
+  CartSlice.actions;
 export default CartSlice.reducer;
